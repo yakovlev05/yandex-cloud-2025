cbe7281b3b9a6e962e549f955ef5035fb95f280b
yakovlev05 <alexey.yakovlev.05@mail.ru>
2025-11-30 13:38:29 +0500

    create spring

---
+++ web/src/test/java/ru/yakovlev/school/web/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yakovlev.school.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
