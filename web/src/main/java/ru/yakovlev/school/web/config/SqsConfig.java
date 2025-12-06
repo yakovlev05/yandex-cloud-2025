9cce45d3c6eec4f1e79f20ebf1e6339fef01e236
yakovlev05 <alexey.yakovlev.05@mail.ru>
2025-12-07 03:04:36 +0500

    add sending generation request

--- web/src/main/java/ru/yakovlev/school/web/config/SqsConfig.java
+++ web/src/main/java/ru/yakovlev/school/web/config/SqsConfig.java
@@ -1,11 +1,14 @@
 package ru.yakovlev.school.web.config;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.awscore.exception.AwsServiceException;
+import software.amazon.awssdk.core.exception.SdkClientException;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.*;
+import software.amazon.awssdk.services.sqs.model.UnsupportedOperationException;
 
 import java.net.URI;
 
@@ -16,11 +19,49 @@ import java.net.URI;
 public class SqsConfig {
 
     @Bean
+    @ConditionalOnProperty(name = "IS_CLOUD_ENV", havingValue = "true")
     public SqsClient sqsClient() {
         return SqsClient.builder()
-                .region(Region.US_EAST_1)
-                .endpointOverride(URI.create("http://localhost:4566"))
+                .region(Region.of("ru-central1"))
+                .endpointOverride(URI.create("https://message-queue.api.cloud.yandex.net"))
                 .build();
+    }
+
+
+    @Bean
+    @ConditionalOnProperty(name = "IS_CLOUD_ENV", havingValue = "false")
+    public SqsClient sqsClientDev() {
+        return new SqsClient() {
+            @Override
+            public String serviceName() {
+                return "";
+            }
+
+            @Override
+            public void close() {
+
+            }
+
+            @Override
+            public GetQueueUrlResponse getQueueUrl(GetQueueUrlRequest getQueueUrlRequest)
+                    throws RequestThrottledException, QueueDoesNotExistException, InvalidAddressException,
+                    InvalidSecurityException, UnsupportedOperationException, AwsServiceException,
+                    SdkClientException, SqsException {
+                return GetQueueUrlResponse.builder()
+                        .queueUrl("dev")
+                        .build();
+            }
+
+            @Override
+            public SendMessageResponse sendMessage(SendMessageRequest sendMessageRequest)
+                    throws InvalidMessageContentsException, UnsupportedOperationException, RequestThrottledException,
+                    QueueDoesNotExistException, InvalidSecurityException, KmsDisabledException,
+                    KmsInvalidStateException, KmsNotFoundException, KmsOptInRequiredException, KmsThrottledException,
+                    KmsAccessDeniedException, KmsInvalidKeyUsageException, InvalidAddressException, AwsServiceException,
+                    SdkClientException, SqsException {
+                return SendMessageResponse.builder().build();
+            }
+        };
     }
 
 }
--- web/src/main/java/ru/yakovlev/school/web/service/ImageService.java
+++ web/src/main/java/ru/yakovlev/school/web/service/ImageService.java
@@ -6,6 +6,7 @@ import ru.yakovlev.school.web.entity.Image;
 import ru.yakovlev.school.web.entity.ImageStatus;
 import ru.yakovlev.school.web.repository.ImageRepository;
 import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.GetQueueUrlRequest;
 import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
 
 import java.util.Optional;
@@ -14,13 +15,16 @@ import java.util.Optional;
 @Service
 public class ImageService {
 
+    private static final String GENERATE_QUEUE = "generate-image";
+
     private final ImageRepository imageRepository;
     private final SqsClient sqsClient;
 
 
     public void generate(Image image) {
         save(image);
-        //TODO: отправка в очередь
+        sendGenerateMessage(image);
+        //https://docs.aws.amazon.com/sdk-for-java/latest/developer-guide/java_sqs_code_examples.html
     }
 
     public void save(Image image) {
@@ -29,5 +33,20 @@ public class ImageService {
 
     public Optional<Image> getExistsById(String id) {
         return imageRepository.findByIdAndStatusNot(id, ImageStatus.DELETED);
+    }
+
+    private void sendGenerateMessage(Image image) {
+        GetQueueUrlRequest getQueueRequest = GetQueueUrlRequest.builder()
+                .queueName(GENERATE_QUEUE)
+                .build();
+
+        String queueUrl = sqsClient.getQueueUrl(getQueueRequest).queueUrl();
+
+        SendMessageRequest sendMessageRequest = SendMessageRequest.builder()
+                .queueUrl(queueUrl)
+                .messageBody("id=%s||prompt=%s".formatted(image.getId(), image.getPrompt()))
+                .build();
+
+        sqsClient.sendMessage(sendMessageRequest);
     }
 }
